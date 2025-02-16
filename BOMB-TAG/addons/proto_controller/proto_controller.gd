3e6e96bc329a63987961f9f00197ee2c8c63fb0a
GMolter <gmolter8@gmail.com>
2025-02-15 19:06:03 -0500

    Added Controller Looking + Sensitivity Adjustment

--- BOMB-TAG/addons/proto_controller/proto_controller.gd
+++ BOMB-TAG/addons/proto_controller/proto_controller.gd
@@ -1,8 +1,3 @@
-# ProtoController v1.0 by Brackeys
-# CC0 License
-# Intended for rapid prototyping of first-person games.
-# Happy prototyping!
-
 extends CharacterBody3D
 
 ## Can we move around?
@@ -19,6 +14,8 @@ extends CharacterBody3D
 @export_group("Speeds")
 ## Look around rotation speed.
 @export var look_speed : float = 0.002
+## Controller look sensitivity
+@export var controller_look_sensitivity : float = 4.0  
 ## Normal speed.
 @export var base_speed : float = 7.0
 ## Speed of jump.
@@ -44,6 +41,12 @@ extends CharacterBody3D
 ## Name of Input Action to toggle freefly mode.
 @export var input_freefly : String = "freefly"
 
+## Controller Look Inputs
+@export var input_look_right : String = "look_right"
+@export var input_look_left : String = "look_left"
+@export var input_look_up : String = "look_up"
+@export var input_look_down : String = "look_down"
+
 var mouse_captured : bool = false
 var look_rotation : Vector2
 var move_speed : float = 0.0
@@ -65,10 +68,10 @@ func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_key_pressed(KEY_ESCAPE):
 		release_mouse()
 	
-	# Look around
+	# Look around (Mouse)
 	if mouse_captured and event is InputEventMouseMotion:
-		rotate_look(event.relative)
-	
+		rotate_look(event.relative * look_speed)
+
 	# Toggle freefly mode
 	if can_freefly and Input.is_action_just_pressed(input_freefly):
 		if not freeflying:
@@ -85,52 +88,45 @@ func _physics_process(delta: float) -> void:
 		move_and_collide(motion)
 		return
 	
-	# Apply gravity to velocity
-	if has_gravity:
-		if not is_on_floor():
-			velocity += get_gravity() * delta
+	# Apply gravity
+	if has_gravity and not is_on_floor():
+		velocity += get_gravity() * delta
 
 	# Apply jumping
-	if can_jump:
-		if Input.is_action_just_pressed(input_jump) and is_on_floor():
-			velocity.y = jump_velocity
+	if can_jump and Input.is_action_just_pressed(input_jump) and is_on_floor():
+		velocity.y = jump_velocity
 
 	# Modify speed based on sprinting
-	if can_sprint and Input.is_action_pressed(input_sprint):
-			move_speed = sprint_speed
-	else:
-		move_speed = base_speed
+	move_speed = sprint_speed if (can_sprint and Input.is_action_pressed(input_sprint)) else base_speed
 
-	# Apply desired movement to velocity
+	# Apply movement
 	if can_move:
 		var input_dir := Input.get_vector(input_left, input_right, input_forward, input_back)
 		var move_dir := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-		if move_dir:
-			velocity.x = move_dir.x * move_speed
-			velocity.z = move_dir.z * move_speed
-		else:
-			velocity.x = move_toward(velocity.x, 0, move_speed)
-			velocity.z = move_toward(velocity.z, 0, move_speed)
+		velocity.x = move_dir.x * move_speed if move_dir else move_toward(velocity.x, 0, move_speed)
+		velocity.z = move_dir.z * move_speed if move_dir else move_toward(velocity.z, 0, move_speed)
 	else:
 		velocity.x = 0
 		velocity.y = 0
 	
-	# Use velocity to actually move
 	move_and_slide()
 
+	# Apply controller look (Right Stick)
+	var look_x = Input.get_action_strength(input_look_right) - Input.get_action_strength(input_look_left)
+	var look_y = Input.get_action_strength(input_look_down) - Input.get_action_strength(input_look_up)
+	if look_x != 0 or look_y != 0:
+		rotate_look(Vector2(look_x, look_y) * controller_look_sensitivity * delta)
 
-## Rotate us to look around.
-## Base of controller rotates around y (left/right). Head rotates around x (up/down).
-## Modifies look_rotation based on rot_input, then resets basis and rotates by look_rotation.
+
+## Rotate look (Mouse & Controller)
 func rotate_look(rot_input : Vector2):
-	look_rotation.x -= rot_input.y * look_speed
+	look_rotation.x -= rot_input.y
 	look_rotation.x = clamp(look_rotation.x, deg_to_rad(-85), deg_to_rad(85))
-	look_rotation.y -= rot_input.x * look_speed
+	look_rotation.y -= rot_input.x
 	transform.basis = Basis()
 	rotate_y(look_rotation.y)
 	head.transform.basis = Basis()
 	head.rotate_x(look_rotation.x)
-
 
 func enable_freefly():
 	collider.disabled = true
@@ -141,38 +137,31 @@ func disable_freefly():
 	collider.disabled = false
 	freeflying = false
 
-
 func capture_mouse():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	mouse_captured = true
-
 
 func release_mouse():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	mouse_captured = false
 
-
 ## Checks if some Input Actions haven't been created.
-## Disables functionality accordingly.
 func check_input_mappings():
-	if can_move and not InputMap.has_action(input_left):
-		push_error("Movement disabled. No InputAction found for input_left: " + input_left)
-		can_move = false
-	if can_move and not InputMap.has_action(input_right):
-		push_error("Movement disabled. No InputAction found for input_right: " + input_right)
-		can_move = false
-	if can_move and not InputMap.has_action(input_forward):
-		push_error("Movement disabled. No InputAction found for input_forward: " + input_forward)
-		can_move = false
-	if can_move and not InputMap.has_action(input_back):
-		push_error("Movement disabled. No InputAction found for input_back: " + input_back)
-		can_move = false
-	if can_jump and not InputMap.has_action(input_jump):
-		push_error("Jumping disabled. No InputAction found for input_jump: " + input_jump)
-		can_jump = false
-	if can_sprint and not InputMap.has_action(input_sprint):
-		push_error("Sprinting disabled. No InputAction found for input_sprint: " + input_sprint)
-		can_sprint = false
-	if can_freefly and not InputMap.has_action(input_freefly):
-		push_error("Freefly disabled. No InputAction found for input_freefly: " + input_freefly)
-		can_freefly = false
+	var actions = {
+		"input_left": input_left,
+		"input_right": input_right,
+		"input_forward": input_forward,
+		"input_back": input_back,
+		"input_jump": input_jump,
+		"input_sprint": input_sprint,
+		"input_freefly": input_freefly,
+		"input_look_right": input_look_right,
+		"input_look_left": input_look_left,
+		"input_look_up": input_look_up,
+		"input_look_down": input_look_down
+	}
+	
+	for key in actions.keys():
+		if not InputMap.has_action(actions[key]):
+			push_error(key + " disabled. No InputAction found for: " + actions[key])
+			set(key, false)
