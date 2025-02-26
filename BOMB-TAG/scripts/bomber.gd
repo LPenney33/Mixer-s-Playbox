21a0ceb58c7f68928b24b70d8cc0b21fd6595d99
GMolter <gmolter8@gmail.com>
2025-02-26 14:09:26 -0500

    Chase function working.
    Next Task: animations.

---
+++ BOMB-TAG/scripts/bomber.gd
@@ -0,0 +1,31 @@
+extends CharacterBody3D
+
+const SPEED = 4.0
+const ATTACK_RANGE = 2.0
+
+@onready var player = get_node("../Map/Player")
+@onready var nav_agent = $NavigationAgent3D
+@onready var anim_player = $zombie/AnimationPlayer
+
+func _ready():
+    if player == null:
+        push_warning("Player not found at '../Map/Player'")
+
+func _physics_process(delta):
+    if player == null:
+        return
+    nav_agent.set_target_position(player.global_transform.origin)
+    var next_nav_point = nav_agent.get_next_path_position()
+    velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+    rotation.y = lerp_angle(rotation.y, atan2(-velocity.x, -velocity.z), delta * 10.0)
+    if global_position.distance_to(player.global_position) < ATTACK_RANGE:
+        anim_player.play("zombie_idle/T-pose")
+        look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
+    else:
+        anim_player.play("zombie_run/Run")
+    move_and_slide()
+
+func _hit_finished():
+    if player and global_position.distance_to(player.global_position) < ATTACK_RANGE + 1.0:
+        var dir = global_position.direction_to(player.global_position)
+        player.hit(dir)
