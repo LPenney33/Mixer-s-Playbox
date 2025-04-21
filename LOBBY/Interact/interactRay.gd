4379db9afef2c07881455851a0ab092960043ea1
14Inderrr <Inder.kanda@yahoo.com>
2025-04-21 14:06:05 -0400

    Finished Transition Torches.

--- LOBBY/Interact/interactRay.gd
+++ LOBBY/Interact/interactRay.gd
@@ -7,5 +7,9 @@ func _physics_process(_delta):
 	
 	if is_colliding():
 		var collider = get_collider()
+		
 		if collider is Interactable:
 			prompt.text = collider.prompt_message
+			
+			if Input.is_action_just_pressed(collider.prompt_input):
+				collider.interact(owner)
